c29df43691a479befafb69906f51b6f3be6af921
ArsatTeguh <arsyatteguh@gmail.com>
2024-12-09 15:42:04 +0700

    add docker

--- helper/corsMidleware.go
+++ helper/corsMidleware.go
@@ -4,7 +4,7 @@ import "github.com/gin-gonic/gin"
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "http://localhost:3000") // Change to your frontend URL
+		c.Header("Access-Control-Allow-Origin", "http://localhost:4173") // Change to your frontend URL
 		c.Header("Access-Control-Allow-Credentials", "true")
 		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH,OPTIONS,GET,PUT,DELETE")
